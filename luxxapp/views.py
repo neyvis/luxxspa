f403115cd06b99ba2cfe01a92542ea4e97b40a41
neyvis <nremon2021@gmail.com>
2022-09-06 11:45:00 -0400

    Adding service list page

--- luxxapp/views.py
+++ luxxapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Service
 
-# Create your views here.
+
+def service_list(request):
+    services = Service.objects.all()
+    return render(request, 'service_list.html', {'services': services})
